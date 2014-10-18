e4899885b4adaeb77374a114b30b13d52f691cea
Mathieu Méa <mathieu.mea@gmail.com>
2014-10-18 11:33:35 -0400

    Remove unused imports.

--- src/org/mtransit/android/commons/provider/BixiBikeStationProvider.java
+++ src/org/mtransit/android/commons/provider/BixiBikeStationProvider.java
@@ -6,10 +6,8 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
-import java.util.Set;
 import java.util.regex.Pattern;
 
 import javax.net.ssl.HttpsURLConnection;
