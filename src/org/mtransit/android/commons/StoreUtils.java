71312b976037babaf4f3d6a16cb628c893475b7d
Mathieu Méa <mathieu.mea@gmail.com>
2015-03-06 10:55:05 -0500

    Open Google Play Store (Android Market) link in the Store instead of the internal web browser.

--- src/org/mtransit/android/commons/StoreUtils.java
+++ src/org/mtransit/android/commons/StoreUtils.java
@@ -12,10 +12,35 @@ public final class StoreUtils implements MTLog.Loggable {
 		return TAG;
 	}
 
-	private static final String GOOGLE_PLAY_BASE_URI_AND_PKG = "market://details?id=%s";
+	private static final String HTTP_SCHEME = "http";
+	private static final String HTTPS_SCHEME = "https";
+	private static final String MARKET_SCHEME = "market";
+	private static final String ANDROID_MARKET_WWW_AUTHORITY = "market.android.com"; // old
+	private static final String GOOGLE_PLAY_STORE_WWW_AUTHORITY = "play.google.com";
+	private static final String GOOGLE_PLAY_STORE_BASE_URI_AND_PKG = MARKET_SCHEME + "://details?id=%s";
+	private static final String GOOGLE_PLAY_STORE_BASE_WWW_URI_AND_PKG = HTTPS_SCHEME + "://play.google.com/store/apps/details?id=%s";
 
 	public static void viewAppPage(Activity activity, String pkg, String label) {
-		Uri uri = Uri.parse(String.format(GOOGLE_PLAY_BASE_URI_AND_PKG, pkg));
-		LinkUtils.open(activity, uri, label);
+		boolean success = LinkUtils.open(activity, Uri.parse(String.format(GOOGLE_PLAY_STORE_BASE_URI_AND_PKG, pkg)), label);
+		if (!success) {
+			LinkUtils.open(activity, Uri.parse(String.format(GOOGLE_PLAY_STORE_BASE_WWW_URI_AND_PKG, pkg)), label);
+		}
+	}
+
+	public static boolean isStoreIntent(String url) {
+		return isStoreIntent(Uri.parse(url));
+	}
+
+	public static boolean isStoreIntent(Uri uri) {
+		if (uri != null) {
+			if (MARKET_SCHEME.equals(uri.getScheme())) {
+				return true;
+			} else if (HTTPS_SCHEME.equals(uri.getScheme()) || HTTP_SCHEME.equals(uri.getScheme())) {
+				if (GOOGLE_PLAY_STORE_WWW_AUTHORITY.equals(uri.getAuthority()) || ANDROID_MARKET_WWW_AUTHORITY.equals(uri.getAuthority())) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 }
