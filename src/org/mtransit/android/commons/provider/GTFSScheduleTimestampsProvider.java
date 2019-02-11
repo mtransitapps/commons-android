b7ec9da1ee6fc685df8fe6fd7f5d269ba7dbfa39
Mathieu Méa <mathieu.mea@gmail.com>
2019-02-10 21:31:07 -0500

    FEATURE: stop times no pick up improvements (WIP)

--- src/org/mtransit/android/commons/provider/GTFSScheduleTimestampsProvider.java
+++ src/org/mtransit/android/commons/provider/GTFSScheduleTimestampsProvider.java
@@ -16,6 +16,7 @@ import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 public class GTFSScheduleTimestampsProvider implements MTLog.Loggable {
 
