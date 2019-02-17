f95c4c9393738af8100bbe4ff51a2959b6abbad6
Mathieu Méa <mathieu.mea@gmail.com>
2019-02-17 05:55:28 -0500

    BashSupport > Convert simple brackets to double brackets

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 echo ">> Building...";
 DIRECTORY=$(basename ${PWD});
 CUSTOM_SETTINGS_GRADLE_FILE="../settings.gradle.all";
-if [ -f $CUSTOM_SETTINGS_GRADLE_FILE ]; then
-	../gradlew -c $CUSTOM_SETTINGS_GRADLE_FILE :$DIRECTORY:clean :$DIRECTORY:assemble;
+if [[ -f ${CUSTOM_SETTINGS_GRADLE_FILE} ]]; then
+	../gradlew -c ${CUSTOM_SETTINGS_GRADLE_FILE} :${DIRECTORY}:clean :${DIRECTORY}:assemble;
 	RESULT=$?;
 else
-	../gradlew :$DIRECTORY:clean :$DIRECTORY:assemble;
+	../gradlew :${DIRECTORY}:clean :${DIRECTORY}:assemble;
 	RESULT=$?;
 fi
 echo ">> Building... DONE";
-exit $RESULT;
+exit ${RESULT};
