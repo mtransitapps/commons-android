0ebb8d26c1dbc8f9375f30a456910fef13bc72d0
Mathieu Méa <mathieu.mea@gmail.com>
2019-05-18 11:31:26 -0400

    HOTFIX: cleanup Span utility class

--- src/org/mtransit/android/commons/SpanUtils.java
+++ src/org/mtransit/android/commons/SpanUtils.java
@@ -2,6 +2,7 @@ package org.mtransit.android.commons;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
 import android.text.style.ForegroundColorSpan;
@@ -10,42 +11,50 @@ import android.text.style.StyleSpan;
 import android.text.style.TextAppearanceSpan;
 import android.text.style.TypefaceSpan;
 
+@SuppressWarnings({"unused", "WeakerAccess"})
 public final class SpanUtils implements MTLog.Loggable {
 
-	private static final String TAG = SpanUtils.class.getSimpleName();
+	private static final String LOG_TAG = SpanUtils.class.getSimpleName();
 
+	@NonNull
 	@Override
 	public String getLogTag() {
-		return TAG;
+		return LOG_TAG;
 	}
 
+	@NonNull
 	public static StyleSpan getNewNormalStyleSpan() {
 		return new StyleSpan(android.graphics.Typeface.NORMAL);
 	}
 
+	@NonNull
 	public static StyleSpan getNewBoldStyleSpan() {
 		return new StyleSpan(android.graphics.Typeface.BOLD);
 	}
 
 	public static final String SANS_SERIF_TYPEFACE = "sans-serif";
 
+	@NonNull
 	public static TypefaceSpan getNewSansSerifTypefaceSpan() {
 		return getNewTypefaceSpan(SANS_SERIF_TYPEFACE);
 	}
 
 	public static final String SANS_SERIF_CONDENSED_TYPEFACE = "sans-serif-condensed";
 
+	@NonNull
 	public static TypefaceSpan getNewSansSerifCondensedTypefaceSpan() {
 		return getNewTypefaceSpan(SANS_SERIF_CONDENSED_TYPEFACE);
 	}
 
 	public static final String SANS_SERIF_LIGHT_TYPEFACE = "sans-serif-light";
 
+	@NonNull
 	public static TypefaceSpan getNewSansSerifLightTypefaceSpan() {
 		return getNewTypefaceSpan(SANS_SERIF_LIGHT_TYPEFACE);
 	}
 
-	public static TypefaceSpan getNewTypefaceSpan(String typeface) {
+	@NonNull
+	public static TypefaceSpan getNewTypefaceSpan(@Nullable String typeface) {
 		return new TypefaceSpan(typeface);
 	}
 
@@ -89,11 +98,13 @@ public final class SpanUtils implements MTLog.Loggable {
 		return new TextAppearanceSpan(context, android.R.style.TextAppearance_Small);
 	}
 
+	@NonNull
 	public static ForegroundColorSpan getNewTextColor(int color) {
 		return new ForegroundColorSpan(color);
 	}
 
-	public static CharSequence setAll(CharSequence cs, Object... spans) {
+	@Nullable
+	public static CharSequence setAll(@Nullable CharSequence cs, @Nullable Object... spans) {
 		if (cs instanceof SpannableStringBuilder) {
 			return setAll((SpannableStringBuilder) cs, spans);
 		} else {
@@ -101,11 +112,13 @@ public final class SpanUtils implements MTLog.Loggable {
 		}
 	}
 
-	public static SpannableStringBuilder setAll(SpannableStringBuilder ssb, Object... spans) {
+	@Nullable
+	public static SpannableStringBuilder setAll(@Nullable SpannableStringBuilder ssb, @Nullable Object... spans) {
 		return set(ssb, 0, ssb == null ? 0 : ssb.length(), spans);
 	}
 
-	public static CharSequence set(CharSequence cs, int start, int end, Object... spans) {
+	@Nullable
+	public static CharSequence set(@Nullable CharSequence cs, int start, int end, @Nullable Object... spans) {
 		if (cs instanceof SpannableStringBuilder) {
 			return set((SpannableStringBuilder) cs, start, end, spans);
 		} else {
@@ -113,9 +126,10 @@ public final class SpanUtils implements MTLog.Loggable {
 		}
 	}
 
-	public static SpannableStringBuilder set(SpannableStringBuilder ssb, int start, int end, Object... spans) {
+	@Nullable
+	public static SpannableStringBuilder set(@Nullable SpannableStringBuilder ssb, int start, int end, @Nullable Object... spans) {
 		if (ssb == null || ssb.length() == 0 || start >= end) {
-			MTLog.w(TAG, "Trying to set span on empty string or %s not before %s!", start, end);
+			MTLog.w(LOG_TAG, "Trying to set span on empty string or %s not before %s!", start, end);
 			return null;
 		}
 		if (spans != null && spans.length > 0) {
