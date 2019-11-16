29257d51bddd90eccb9690d02b9cf34fed6b2b1f
Mathieu Méa <mathieu.mea@gmail.com>
2019-11-16 09:25:30 -0500

    Improve gradle settings for CI

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+source ../commons/commons.sh
 echo ">> Building...";
+setGradleArgs;
+echo "GRADLE_ARGS: $GRADLE_ARGS";
 DIRECTORY=$(basename ${PWD});
 CUSTOM_SETTINGS_GRADLE_FILE="../settings.gradle.all";
 if [[ -f ${CUSTOM_SETTINGS_GRADLE_FILE} ]]; then
-	../gradlew -c ${CUSTOM_SETTINGS_GRADLE_FILE} :${DIRECTORY}:clean :${DIRECTORY}:assemble;
+	../gradlew -c ${CUSTOM_SETTINGS_GRADLE_FILE} :${DIRECTORY}:clean :${DIRECTORY}:assemble ${GRADLE_ARGS};
 	RESULT=$?;
 else
-	../gradlew :${DIRECTORY}:clean :${DIRECTORY}:assemble;
+	../gradlew :${DIRECTORY}:clean :${DIRECTORY}:assemble ${GRADLE_ARGS};
 	RESULT=$?;
 fi
 echo ">> Building... DONE";
