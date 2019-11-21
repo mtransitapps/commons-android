285fe3738de0a9a7a53afaf33a715f910ad9e11d
Mathieu Méa <mathieu.mea@gmail.com>
2019-11-20 20:23:13 -0500

    Fix Regina Transit real-time schedule status provider time parsing

--- src/main/java/org/mtransit/android/commons/provider/ReginaTransitProvider.java
+++ src/main/java/org/mtransit/android/commons/provider/ReginaTransitProvider.java
@@ -1,17 +1,18 @@
 package org.mtransit.android.commons.provider;
 
-import java.net.HttpURLConnection;
-import java.net.SocketException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Collection;
-import java.util.Locale;
-import java.util.TimeZone;
-import java.util.concurrent.TimeUnit;
-import java.util.regex.Pattern;
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
+import android.text.TextUtils;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.VisibleForTesting;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -30,75 +31,86 @@ import org.mtransit.android.commons.data.RouteTripStop;
 import org.mtransit.android.commons.data.Schedule;
 import org.mtransit.android.commons.data.Trip;
 
-import android.annotation.SuppressLint;
-import android.content.ContentValues;
-import android.content.Context;
-import android.content.UriMatcher;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-import android.net.Uri;
-import android.text.TextUtils;
-
-import androidx.annotation.NonNull;
+import java.net.HttpURLConnection;
+import java.net.SocketException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.UnknownHostException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Pattern;
 
 @SuppressLint("Registered")
 public class ReginaTransitProvider extends MTContentProvider implements StatusProviderContract {
 
-	private static final String TAG = ReginaTransitProvider.class.getSimpleName();
+	private static final String LOG_TAG = ReginaTransitProvider.class.getSimpleName();
 
+	@NonNull
 	@Override
 	public String getLogTag() {
-		return TAG;
+		return LOG_TAG;
 	}
 
-	public static UriMatcher getNewUriMatcher(String authority) {
+	@NonNull
+	public static UriMatcher getNewUriMatcher(@NonNull String authority) {
 		UriMatcher URI_MATCHER = new UriMatcher(UriMatcher.NO_MATCH);
 		StatusProvider.append(URI_MATCHER, authority);
 		return URI_MATCHER;
 	}
 
+	@Nullable
 	private static UriMatcher uriMatcher = null;
 
 	/**
 	 * Override if multiple {@link ReginaTransitProvider} implementations in same app.
 	 */
-	private static UriMatcher getURIMATCHER(Context context) {
+	@NonNull
+	private static UriMatcher getURIMATCHER(@NonNull Context context) {
 		if (uriMatcher == null) {
 			uriMatcher = getNewUriMatcher(getAUTHORITY(context));
 		}
 		return uriMatcher;
 	}
 
+	@Nullable
 	private static String authority = null;
 
 	/**
 	 * Override if multiple {@link ReginaTransitProvider} implementations in same app.
 	 */
-	private static String getAUTHORITY(Context context) {
+	@NonNull
+	private static String getAUTHORITY(@NonNull Context context) {
 		if (authority == null) {
 			authority = context.getResources().getString(R.string.regina_transit_authority);
 		}
 		return authority;
 	}
 
+	@Nullable
 	private static Uri authorityUri = null;
 
 	/**
 	 * Override if multiple {@link ReginaTransitProvider} implementations in same app.
 	 */
-	private static Uri getAUTHORITY_URI(Context context) {
+	@NonNull
+	private static Uri getAUTHORITY_URI(@NonNull Context context) {
 		if (authorityUri == null) {
 			authorityUri = UriUtils.newContentUri(getAUTHORITY(context));
 		}
 		return authorityUri;
 	}
 
-	private static final long TRANSIT_LIVE_STATUS_MAX_VALIDITY_IN_MS = TimeUnit.HOURS.toMillis(1);
-	private static final long TRANSIT_LIVE_STATUS_VALIDITY_IN_MS = TimeUnit.MINUTES.toMillis(10);
-	private static final long TRANSIT_LIVE_STATUS_VALIDITY_IN_FOCUS_IN_MS = TimeUnit.MINUTES.toMillis(1);
-	private static final long TRANSIT_LIVE_STATUS_MIN_DURATION_BETWEEN_REFRESH_IN_MS = TimeUnit.MINUTES.toMillis(1);
-	private static final long TRANSIT_LIVE_STATUS_MIN_DURATION_BETWEEN_REFRESH_IN_FOCUS_IN_MS = TimeUnit.MINUTES.toMillis(1);
+	private static final long TRANSIT_LIVE_STATUS_MAX_VALIDITY_IN_MS = TimeUnit.HOURS.toMillis(1L);
+	private static final long TRANSIT_LIVE_STATUS_VALIDITY_IN_MS = TimeUnit.MINUTES.toMillis(10L);
+	private static final long TRANSIT_LIVE_STATUS_VALIDITY_IN_FOCUS_IN_MS = TimeUnit.MINUTES.toMillis(1L);
+	private static final long TRANSIT_LIVE_STATUS_MIN_DURATION_BETWEEN_REFRESH_IN_MS = TimeUnit.MINUTES.toMillis(1L);
+	private static final long TRANSIT_LIVE_STATUS_MIN_DURATION_BETWEEN_REFRESH_IN_FOCUS_IN_MS = TimeUnit.MINUTES.toMillis(1L);
 
 	@Override
 	public long getStatusMaxValidityInMs() {
@@ -122,14 +134,15 @@ public class ReginaTransitProvider extends MTContentProvider implements StatusPr
 	}
 
 	@Override
-	public void cacheStatus(POIStatus newStatusToCache) {
+	public void cacheStatus(@NonNull POIStatus newStatusToCache) {
 		StatusProvider.cacheStatusS(this, newStatusToCache);
 	}
 
+	@Nullable
 	@Override
-	public POIStatus getCachedStatus(StatusProviderContract.Filter statusFilter) {
+	public POIStatus getCachedStatus(@NonNull StatusProviderContract.Filter statusFilter) {
 		if (!(statusFilter instanceof Schedule.ScheduleStatusFilter)) {
-			MTLog.w(this, "getNewStatus() > Can't find new schecule whithout schedule filter!");
+			MTLog.w(this, "getNewStatus() > Can't find new schedule without schedule filter!");
 			return null;
 		}
 		Schedule.ScheduleStatusFilter scheduleStatusFilter = (Schedule.ScheduleStatusFilter) statusFilter;
@@ -154,6 +167,7 @@ public class ReginaTransitProvider extends MTContentProvider implements StatusPr
 		return StatusProvider.deleteCachedStatus(this, cachedStatusId);
 	}
 
+	@NonNull
 	@Override
 	public String getStatusDbTableName() {
 		return ReginaTransitDbHelper.T_TRANSIT_LIVE_STATUS;
@@ -165,9 +179,9 @@ public class ReginaTransitProvider extends MTContentProvider implements StatusPr
 	}
 
 	@Override
-	public POIStatus getNewStatus(StatusProviderContract.Filter statusFilter) {
+	public POIStatus getNewStatus(@NonNull StatusProviderContract.Filter statusFilter) {
 		if (!(statusFilter instanceof Schedule.ScheduleStatusFilter)) {
-			MTLog.w(this, "getNewStatus() > Can't find new schecule whithout schedule filter!");
+			MTLog.w(this, "getNewStatus() > Can't find new schedule without schedule filter!");
 			return null;
 		}
 		Schedule.ScheduleStatusFilter scheduleStatusFilter = (Schedule.ScheduleStatusFilter) statusFilter;
@@ -178,12 +192,12 @@ public class ReginaTransitProvider extends MTContentProvider implements StatusPr
 
 	private static final TimeZone REGINA_TZ = TimeZone.getTimeZone("America/Regina");
 
-
 	private static final TimeZone UTC_TZ = TimeZone.getTimeZone("UTC");
 
 	private static final ThreadSafeDateFormatter DATE_FORMATTER_UTC;
+
 	static {
-		ThreadSafeDateFormatter dateFormatter = new ThreadSafeDateFormatter("hh:mm a");
+		ThreadSafeDateFormatter dateFormatter = new ThreadSafeDateFormatter("hh:mm a", Locale.ENGLISH);
 		dateFormatter.setTimeZone(UTC_TZ);
 		DATE_FORMATTER_UTC = dateFormatter;
 	}
@@ -192,19 +206,18 @@ public class ReginaTransitProvider extends MTContentProvider implements StatusPr
 	private static final String REAL_TIME_URL_PART_2_BEFORE_ROUTE_SHORT_NAME = "&routes=";
 	private static final String REAL_TIME_URL_PART_3 = "&lim=7";
 
-	private static String getRealTimeStatusUrlString(Context context, RouteTripStop rts) {
-		return new StringBuilder() //
-				.append(REAL_TIME_URL_PART_1_BEFORE_STOP_CODE) //
-				.append(rts.getStop().getCode()) //
-				.append(REAL_TIME_URL_PART_2_BEFORE_ROUTE_SHORT_NAME) //
-				.append(rts.getRoute().getShortName()) //
-				.append(REAL_TIME_URL_PART_3) //
-				.toString();
+	private static String getRealTimeStatusUrlString(@NonNull RouteTripStop rts) {
+		return REAL_TIME_URL_PART_1_BEFORE_STOP_CODE + //
+				rts.getStop().getCode() + //
+				REAL_TIME_URL_PART_2_BEFORE_ROUTE_SHORT_NAME + //
+				rts.getRoute().getShortName() + //
+				REAL_TIME_URL_PART_3;
 	}
 
-	private void loadRealTimeStatusFromWWW(RouteTripStop rts) {
+	private void loadRealTimeStatusFromWWW(@NonNull RouteTripStop rts) {
 		try {
-			String urlString = getRealTimeStatusUrlString(getContext(), rts);
+			String urlString = getRealTimeStatusUrlString(rts);
+			MTLog.i(this, "Loading from '%s'...", urlString);
 			URL url = new URL(urlString);
 			URLConnection urlc = url.openConnection();
 			HttpURLConnection httpUrlConnection = (HttpURLConnection) urlc;
@@ -231,13 +244,16 @@ public class ReginaTransitProvider extends MTContentProvider implements StatusPr
 				MTLog.w(this, "No Internet Connection!");
 			}
 		} catch (SocketException se) {
-			MTLog.w(TAG, se, "No Internet Connection!");
+			MTLog.w(LOG_TAG, se, "No Internet Connection!");
 		} catch (Exception e) { // Unknown error
-			MTLog.e(TAG, e, "INTERNAL ERROR: Unknown Exception");
+			MTLog.e(LOG_TAG, e, "INTERNAL ERROR: Unknown Exception");
 		}
 	}
 
-	private static long PROVIDER_PRECISION_IN_MS = TimeUnit.SECONDS.toMillis(10);
+	private static long PROVIDER_PRECISION_IN_MS = TimeUnit.SECONDS.toMillis(10L);
+
+	private static final long ONE_DAY = TimeUnit.DAYS.toMillis(1L);
+	private static final long ONE_HOUR = TimeUnit.HOURS.toMillis(1L);
 
 	private static final String JSON_PRED_TIME = "pred_time";
 	private static final String JSON_LINE_NAME = "line_name";
@@ -255,20 +271,17 @@ public class ReginaTransitProvider extends MTContentProvider implements StatusPr
 				beginningOfTodayCal.set(Calendar.SECOND, 0);
 				beginningOfTodayCal.set(Calendar.MILLISECOND, 0);
 				long beginningOfTodayMs = beginningOfTodayCal.getTimeInMillis();
-				long after = newLastUpdateInMs - TimeUnit.HOURS.toMillis(1);
+				long after = newLastUpdateInMs - ONE_HOUR;
 				for (int s = 0; s < json.length(); s++) {
 					JSONObject j = json.getJSONObject(s);
 					if (j == null) {
 						continue;
 					}
 					if (j.has(JSON_PRED_TIME)) {
-						String predictionTimeS = j.getString(JSON_PRED_TIME);
-						if (TextUtils.isEmpty(predictionTimeS)) {
-							continue;
-						}
-						long t = beginningOfTodayMs + TimeUtils.timeToTheTensSecondsMillis(DATE_FORMATTER_UTC.parseThreadSafe(predictionTimeS).getTime());
+						final long time = parseTime(j.getString(JSON_PRED_TIME));
+						long t = beginningOfTodayMs + TimeUtils.timeToTheTensSecondsMillis(time);
 						if (t < after) {
-							t += TimeUnit.DAYS.toMillis(1); // TOMORROW
+							t += ONE_DAY; // TOMORROW
 						}
 						Schedule.Timestamp timestamp = new Schedule.Timestamp(t);
 						try {
@@ -294,47 +307,59 @@ public class ReginaTransitProvider extends MTContentProvider implements StatusPr
 		}
 	}
 
-	private static final Pattern ALBERT_NORTH = Pattern.compile("((^|\\W){1}(albert north)(\\W|$){1})", Pattern.CASE_INSENSITIVE);
+	@VisibleForTesting
+	protected static long parseTime(@Nullable String predictionTimeS) throws ParseException {
+		if (predictionTimeS == null || predictionTimeS.isEmpty()) {
+			return -1L;
+		}
+		final Date date = DATE_FORMATTER_UTC.parseThreadSafe(predictionTimeS);
+		if (date == null) {
+			return -1L;
+		}
+		return date.getTime();
+	}
+
+	private static final Pattern ALBERT_NORTH = Pattern.compile("((^|\\W)(albert north)(\\W|$))", Pattern.CASE_INSENSITIVE);
 	private static final String ALBERT_NORTH_REPLACEMENT = "$2North$4";
 
-	private static final Pattern ALBERT_SOUTH = Pattern.compile("((^|\\W){1}(albert south)(\\W|$){1})", Pattern.CASE_INSENSITIVE);
+	private static final Pattern ALBERT_SOUTH = Pattern.compile("((^|\\W)(albert south)(\\W|$))", Pattern.CASE_INSENSITIVE);
 	private static final String ALBERT_SOUTH_REPLACEMENT = "$2South$4";
 
 	private static final String DOWNTOWN = "Downtown";
 
-	private static final Pattern DOWN = Pattern.compile("((^|\\W){1}(down)(\\W|$){1})", Pattern.CASE_INSENSITIVE);
+	private static final Pattern DOWN = Pattern.compile("((^|\\W)(down)(\\W|$))", Pattern.CASE_INSENSITIVE);
 	private static final String DOWN_REPLACEMENT = "$2" + DOWNTOWN + "$4";
 
 	private static final String INDUSTRIAL_SHORT = "Ind";
 
-	private static final Pattern INDUST = Pattern.compile("((^|\\W){1}(indust)(\\W|$){1})", Pattern.CASE_INSENSITIVE);
+	private static final Pattern INDUST = Pattern.compile("((^|\\W)(indust)(\\W|$))", Pattern.CASE_INSENSITIVE);
 	private static final String INDUST_REPLACEMENT = "$2" + INDUSTRIAL_SHORT + "$4";
 
-	private static final Pattern INDUSTRIAL = Pattern.compile("((^|\\W){1}(industrial)(\\W|$){1})", Pattern.CASE_INSENSITIVE);
+	private static final Pattern INDUSTRIAL = Pattern.compile("((^|\\W)(industrial)(\\W|$))", Pattern.CASE_INSENSITIVE);
 	private static final String INDUSTRIAL_REPLACEMENT = "$2" + INDUSTRIAL_SHORT + "$4";
 
-	private static final Pattern LAND = Pattern.compile("((^|\\W){1}(land)(\\W|$){1})", Pattern.CASE_INSENSITIVE);
+	private static final Pattern LAND = Pattern.compile("((^|\\W)(land)(\\W|$))", Pattern.CASE_INSENSITIVE);
 	private static final String LAND_REPLACEMENT = "$2Landing$4";
 
-	private static final Pattern MED = Pattern.compile("((^|\\W){1}(med)(\\W|$){1})", Pattern.CASE_INSENSITIVE);
+	private static final Pattern MED = Pattern.compile("((^|\\W)(med)(\\W|$))", Pattern.CASE_INSENSITIVE);
 	private static final String MED_REPLACEMENT = "$2Meadows$4";
 
-	private static final Pattern NOR_HEIGHTS = Pattern.compile("((^|\\W){1}(nor\\.heights)(\\W|$){1})", Pattern.CASE_INSENSITIVE);
+	private static final Pattern NOR_HEIGHTS = Pattern.compile("((^|\\W)(nor\\.heights)(\\W|$))", Pattern.CASE_INSENSITIVE);
 	private static final String NOR_HEIGHTS_REPLACEMENT = "$2Normandy Heights$4";
 
-	private static final Pattern NOR = Pattern.compile("((^|\\W){1}(nor)(\\W|$){1})", Pattern.CASE_INSENSITIVE);
+	private static final Pattern NOR = Pattern.compile("((^|\\W)(nor)(\\W|$))", Pattern.CASE_INSENSITIVE);
 	private static final String NOR_REPLACEMENT = "$2Normandy$4";
 
-	private static final Pattern VIC_DOWNTOWN = Pattern.compile("((^|\\W){1}(vic downtown)(\\W|$){1})", Pattern.CASE_INSENSITIVE);
+	private static final Pattern VIC_DOWNTOWN = Pattern.compile("((^|\\W)(vic downtown)(\\W|$))", Pattern.CASE_INSENSITIVE);
 	private static final String VIC_DOWNTOWN_REPLACEMENT = "$2" + DOWNTOWN + "$4";
 
-	private static final Pattern VIC_EAST = Pattern.compile("((^|\\W){1}(vic east)(\\W|$){1})", Pattern.CASE_INSENSITIVE);
+	private static final Pattern VIC_EAST = Pattern.compile("((^|\\W)(vic east)(\\W|$))", Pattern.CASE_INSENSITIVE);
 	private static final String VIC_EAST_REPLACEMENT = "$2East$4";
 
-	private static final Pattern WHIT = Pattern.compile("((^|\\W){1}(whit)(\\W|$){1})", Pattern.CASE_INSENSITIVE);
+	private static final Pattern WHIT = Pattern.compile("((^|\\W)(whit)(\\W|$))", Pattern.CASE_INSENSITIVE);
 	private static final String WHIT_REPLACEMENT = "$2Whitmore$4";
 
-	private static final Pattern WOOD = Pattern.compile("((^|\\W){1}(wood)(\\W|$){1})", Pattern.CASE_INSENSITIVE);
+	private static final Pattern WOOD = Pattern.compile("((^|\\W)(wood)(\\W|$))", Pattern.CASE_INSENSITIVE);
 	private static final String WOOD_REPLACEMENT = "$2woodland$4";
 
 	private String cleanTripHeadsign(String tripHeadsign) {
@@ -373,11 +398,13 @@ public class ReginaTransitProvider extends MTContentProvider implements StatusPr
 		// DO NOTHING
 	}
 
-	private static ReginaTransitDbHelper dbHelper;
+	@Nullable
+	private ReginaTransitDbHelper dbHelper;
 
 	private static int currentDbVersion = -1;
 
-	private ReginaTransitDbHelper getDBHelper(Context context) {
+	@NonNull
+	private ReginaTransitDbHelper getDBHelper(@NonNull Context context) {
 		if (dbHelper == null) { // initialize
 			dbHelper = getNewDbHelper(context);
 			currentDbVersion = getCurrentDbVersion();
@@ -399,28 +426,36 @@ public class ReginaTransitProvider extends MTContentProvider implements StatusPr
 	 * Override if multiple {@link ReginaTransitProvider} implementations in same app.
 	 */
 	public int getCurrentDbVersion() {
+		//noinspection ConstantConditions // TODO requireContext()
 		return ReginaTransitDbHelper.getDbVersion(getContext());
 	}
 
 	/**
 	 * Override if multiple {@link ReginaTransitProvider} implementations in same app.
 	 */
+	@NonNull
 	public ReginaTransitDbHelper getNewDbHelper(Context context) {
 		return new ReginaTransitDbHelper(context.getApplicationContext());
 	}
 
+	@NonNull
 	@Override
 	public UriMatcher getURI_MATCHER() {
+		//noinspection ConstantConditions // TODO requireContext()
 		return getURIMATCHER(getContext());
 	}
 
+	@NonNull
 	@Override
 	public Uri getAuthorityUri() {
+		//noinspection ConstantConditions // TODO requireContext()
 		return getAUTHORITY_URI(getContext());
 	}
 
+	@NonNull
 	@Override
 	public SQLiteOpenHelper getDBHelper() {
+		//noinspection ConstantConditions // TODO requireContext()
 		return getDBHelper(getContext());
 	}
 
@@ -462,11 +497,12 @@ public class ReginaTransitProvider extends MTContentProvider implements StatusPr
 
 	public static class ReginaTransitDbHelper extends MTSQLiteOpenHelper {
 
-		private static final String TAG = ReginaTransitDbHelper.class.getSimpleName();
+		private static final String LOG_TAG = ReginaTransitDbHelper.class.getSimpleName();
 
+		@NonNull
 		@Override
 		public String getLogTag() {
-			return TAG;
+			return LOG_TAG;
 		}
 
 		/**
@@ -474,7 +510,7 @@ public class ReginaTransitProvider extends MTContentProvider implements StatusPr
 		 */
 		protected static final String DB_NAME = "reginatransit.db";
 
-		public static final String T_TRANSIT_LIVE_STATUS = StatusProvider.StatusDbHelper.T_STATUS;
+		static final String T_TRANSIT_LIVE_STATUS = StatusProvider.StatusDbHelper.T_STATUS;
 
 		private static final String T_TRANSIT_LIVE_STATUS_SQL_CREATE = StatusProvider.StatusDbHelper.getSqlCreateBuilder(T_TRANSIT_LIVE_STATUS).build();
 
@@ -485,33 +521,33 @@ public class ReginaTransitProvider extends MTContentProvider implements StatusPr
 		/**
 		 * Override if multiple {@link ReginaTransitDbHelper} in same app.
 		 */
-		public static int getDbVersion(Context context) {
+		public static int getDbVersion(@NonNull Context context) {
 			if (dbVersion < 0) {
 				dbVersion = context.getResources().getInteger(R.integer.regina_transit_db_version);
 			}
 			return dbVersion;
 		}
 
-		public ReginaTransitDbHelper(Context context) {
+		ReginaTransitDbHelper(@NonNull Context context) {
 			super(context, DB_NAME, null, getDbVersion(context));
 		}
 
 		@Override
-		public void onCreateMT(SQLiteDatabase db) {
+		public void onCreateMT(@NonNull SQLiteDatabase db) {
 			initAllDbTables(db);
 		}
 
 		@Override
-		public void onUpgradeMT(SQLiteDatabase db, int oldVersion, int newVersion) {
+		public void onUpgradeMT(@NonNull SQLiteDatabase db, int oldVersion, int newVersion) {
 			db.execSQL(T_TRANSIT_LIVE_STATUS_SQL_DROP);
 			initAllDbTables(db);
 		}
 
-		public boolean isDbExist(Context context) {
+		public boolean isDbExist(@NonNull Context context) {
 			return SqlUtils.isDbExist(context, DB_NAME);
 		}
 
-		private void initAllDbTables(SQLiteDatabase db) {
+		private void initAllDbTables(@NonNull SQLiteDatabase db) {
 			db.execSQL(T_TRANSIT_LIVE_STATUS_SQL_CREATE);
 		}
 	}
