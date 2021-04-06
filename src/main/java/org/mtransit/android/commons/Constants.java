ab198144212d6fb9e8141cd9af427979cc95f3fc
Mathieu Méa <mathieu.mea@gmail.com>
2021-04-06 15:58:35 -0400

    Disable strict mode by default for now

--- src/main/java/org/mtransit/android/commons/Constants.java
+++ src/main/java/org/mtransit/android/commons/Constants.java
@@ -8,8 +8,8 @@ public final class Constants {
 	public static boolean DEBUG = false;
 	// public static boolean DEBUG = true; // DEBUG
 
-	public static boolean FORCE_STRICT_MODE_OFF = false;
-	// public static boolean FORCE_STRICT_MODE_OFF = true; // DEBUG
+	// public static boolean FORCE_STRICT_MODE_OFF = false;
+	public static boolean FORCE_STRICT_MODE_OFF = true; // DEBUG
 
 	public static boolean FORCE_NEARBY_POI_LIST_OFF = false;
 	// public static boolean FORCE_NEARBY_POI_LIST_OFF = true; // DEBUG
