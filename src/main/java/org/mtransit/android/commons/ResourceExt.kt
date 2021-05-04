318fb825049ede182b2bbc016e6350addbcfcbc6
Mathieu Méa <mathieu.mea@gmail.com>
2021-05-04 09:33:02 -0400

    Add Extensions

--- src/main/java/org/mtransit/android/commons/ResourceExt.kt
+++ src/main/java/org/mtransit/android/commons/ResourceExt.kt
@@ -1,8 +1,13 @@
 package org.mtransit.android.commons
 
 import android.content.res.Resources
+import androidx.annotation.DimenRes
 
 val Int.dp: Int
     get() = (this * Resources.getSystem().displayMetrics.density).toInt()
 val Int.px: Int
     get() = (this / Resources.getSystem().displayMetrics.density).toInt()
+
+fun Resources.getDimensionInt(@DimenRes id: Int): Int {
+    return this.getDimension(id).toInt()
+}
