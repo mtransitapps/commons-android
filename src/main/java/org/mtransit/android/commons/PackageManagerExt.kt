e474e54f085c1280c033f84f4c7ab441fcba7803
Mathieu Méa <mathieu.mea@gmail.com>
2022-11-24 14:08:56 -0500

    PackageManager > fix deprecation on API Level 33+

--- src/main/java/org/mtransit/android/commons/PackageManagerExt.kt
+++ src/main/java/org/mtransit/android/commons/PackageManagerExt.kt
@@ -1,16 +1,41 @@
+@file:Suppress("unused")
+
 package org.mtransit.android.commons
 
 import android.annotation.SuppressLint
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
 import android.content.pm.ProviderInfo
+import android.os.Build
 import androidx.core.content.pm.PackageInfoCompat
 
 const val LOG_TAG = "PackageManagerExt"
 
+// https://developer.android.com/reference/androidx/core/content/PackageManagerCompat
+// https://developer.android.com/reference/androidx/core/content/pm/PackageInfoCompat
+// https://developers.google.com/android/reference/com/google/android/gms/instantapps/PackageManagerCompat
+fun PackageManager.getPackageInfoCompat(pkg: String, flags: Int): PackageInfo {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        this.getPackageInfo(pkg, PackageManager.PackageInfoFlags.of(flags.toLong()))
+    } else {
+        @Suppress("DEPRECATION")
+        this.getPackageInfo(pkg, flags)
+    }
+}
+
+@SuppressLint("QueryPermissionsNeeded")
+fun PackageManager.getInstalledPackagesCompat(flags: Int): List<PackageInfo> {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        this.getInstalledPackages(PackageManager.PackageInfoFlags.of(flags.toLong()))
+    } else {
+        @Suppress("DEPRECATION")
+        this.getInstalledPackages(flags)
+    }
+}
+
 fun PackageManager.isAppInstalled(pkg: String): Boolean {
     return try {
-        this.getPackageInfo(pkg, PackageManager.GET_ACTIVITIES)
+        this.getPackageInfoCompat(pkg, PackageManager.GET_ACTIVITIES)
         true
     } catch (e: PackageManager.NameNotFoundException) {
         false
@@ -29,7 +54,7 @@ fun PackageManager.isAppEnabled(pkg: String): Boolean {
 
 fun PackageManager.getAppLongVersionCode(pkg: String, default: Long = -1L): Long {
     return try {
-        PackageInfoCompat.getLongVersionCode(this.getPackageInfo(pkg, 0))
+        PackageInfoCompat.getLongVersionCode(this.getPackageInfoCompat(pkg, 0))
     } catch (e: PackageManager.NameNotFoundException) {
         MTLog.w(LOG_TAG, e, "Error while looking up '%s' version code!", pkg)
         default
@@ -39,7 +64,7 @@ fun PackageManager.getAppLongVersionCode(pkg: String, default: Long = -1L): Long
 @SuppressLint("QueryPermissionsNeeded")
 fun PackageManager.getAllInstalledProvidersWithMetaData(): List<PackageInfo> {
     return try {
-        this.getInstalledPackages(PackageManager.GET_PROVIDERS or PackageManager.GET_META_DATA).toList()
+        this.getInstalledPackagesCompat(PackageManager.GET_PROVIDERS or PackageManager.GET_META_DATA).toList()
     } catch (e: Exception) {
         MTLog.w(LOG_TAG, e, "Error while reading installed providers w/ meta-data!") // #Android5 #Android6
         emptyList()
