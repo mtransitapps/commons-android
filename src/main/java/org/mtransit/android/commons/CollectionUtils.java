31951b42097a0afde5a9ac5ad85cc6a79cd05f89
Mathieu Méa <mathieu.mea@gmail.com>
2022-12-03 10:35:25 -0500

    Move CollectionUtils from common-android to commons-java

--- src/main/java/org/mtransit/android/commons/CollectionUtils.java
+++ src/main/java/org/mtransit/android/commons/CollectionUtils.java
@@ -4,16 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.util.Predicate;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 
-@SuppressWarnings("UnusedReturnValue")
+@Deprecated
+@SuppressWarnings({"UnusedReturnValue", "unused"})
 public final class CollectionUtils implements MTLog.Loggable {
 
 	private static final String LOG_TAG = CollectionUtils.class.getSimpleName();
@@ -28,57 +25,32 @@ public final class CollectionUtils implements MTLog.Loggable {
 	}
 
 	public static int getSize(final @Nullable Collection<?> collection) {
-		if (collection == null) {
-			return 0;
-		}
-		return collection.size();
+		return org.mtransit.commons.CollectionUtils.getSize(collection);
 	}
 
 	public static int getSize(final @Nullable Map<?, ?> map) {
-		if (map == null) {
-			return 0;
-		}
-		return map.size();
+		return org.mtransit.commons.CollectionUtils.getSize(map);
 	}
 
 	public static <T> void sort(@Nullable java.util.List<T> list, @NonNull Comparator<? super T> comparator) {
-		if (list == null || list.size() < 2) {
-			return; // nothing to sort if null / empty / 1 element
-		}
-		Collections.sort(list, comparator);
+		org.mtransit.commons.CollectionUtils.sort(list, comparator);
 	}
 
 	@Nullable
-	public static <T> T getOrNull(@Nullable List<T> list, int i) {
-		try {
-			if (list == null) {
-				return null;
-			}
-			if (i < list.size()) {
-				return list.get(i);
-			} else {
-				return null;
-			}
-		} catch (Exception e) {
-			MTLog.d(LOG_TAG, e, "Error while reading object '%d' from '%s'!", i, list);
-			return null;
-		}
+	public static <T> T getOrNull(@Nullable List<T> list, int index) {
+		return org.mtransit.commons.CollectionUtils.getOrNull(list, index);
 	}
 
 	@NonNull
 	public static <T> List<T> removeDuplicates(@NonNull List<T> list) {
-		return new ArrayList<>(new LinkedHashSet<>(list));
+		return org.mtransit.commons.CollectionUtils.removeDuplicatesNN(list);
 	}
 
 	public static <T> int removeIf(@NonNull List<T> list, @NonNull Predicate<? super T> filter) {
-		int removed = 0;
-		final Iterator<T> each = list.listIterator();
-		while (each.hasNext()) {
-			if (filter.test(each.next())) {
-				each.remove();
-				removed++;
-			}
-		}
-		return removed;
+		return org.mtransit.commons.CollectionUtils.removeIfNN(list, filter::test);
+	}
+
+	private static <T> boolean equalsList(List<T> l1, List<T> l2) {
+		return org.mtransit.commons.CollectionUtils.equalsList(l1, l2);
 	}
 }
