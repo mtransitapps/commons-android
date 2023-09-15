dbc2c6364f17df88a850994d0cc300699a575cf2
Mathieu Méa <mathieu.mea@gmail.com>
2023-09-15 16:50:12 -0400

    Update GTFS-RT proto file

--- download-gtfs-realtime-proto.sh
+++ download-gtfs-realtime-proto.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 source ../commons/commons.sh
 echo ">> Downloading GTFS Real-Time proto..."
-URL="https://developers.google.com/transit/gtfs-realtime/gtfs-realtime.proto"
+# URL="https://developers.google.com/transit/gtfs-realtime/gtfs-realtime.proto"
+# URL="https://raw.githubusercontent.com/google/transit/master/gtfs-realtime/proto/gtfs-realtime.proto"
+URL="https://gtfs.org/realtime/gtfs-realtime.proto"
 PROTO_FILE="src/main/proto/gtfs-realtime.proto"
 download "${URL}" "${PROTO_FILE}"
 checkResult $?
