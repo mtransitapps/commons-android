454406dfa902e2c88de60af4c4c9b58cc10c627b
Mathieu Méa <mathieu.mea@gmail.com>
2023-12-11 17:29:02 -0500

    Add hide booking required local preference

--- src/test/java/org/mtransit/android/commons/provider/CaLTCOnlineProviderTest.java
+++ src/test/java/org/mtransit/android/commons/provider/CaLTCOnlineProviderTest.java
@@ -32,7 +32,7 @@ public class CaLTCOnlineProviderTest {
 
 	private static final String AUTHORITY = "authority.test";
 
-	private static final Route DEFAULT_ROUTE = new Route(1, "1", "route 1", "color", 1);
+	private static final Route DEFAULT_ROUTE = new Route(1, "1", "route 1", "color", 1, 0);
 	private static final Trip DEFAULT_TRIP = new Trip(1, Trip.HEADSIGN_TYPE_STRING, "trip 1", 1);
 	private static final Stop DEFAULT_STOP = new Stop(1, "1", "stop 1", 0, 0, 0, 1);
 
