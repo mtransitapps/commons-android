0c50166aefc5da29168d3dc36ddc602cfbfcdda8
Mathieu Méa <mathieu.mea@gmail.com>
2024-12-16 19:53:57 -0500

    dpToPx

--- src/main/java/org/mtransit/android/commons/ResourceExt.kt
+++ src/main/java/org/mtransit/android/commons/ResourceExt.kt
@@ -2,11 +2,17 @@ package org.mtransit.android.commons
 
 import android.content.res.Resources
 import androidx.annotation.DimenRes
+import androidx.annotation.Px
 
 val Int.dp: Int
     get() = (this * Resources.getSystem().displayMetrics.density).toInt()
+
+@get:Px
 val Int.px: Int
     get() = (this / Resources.getSystem().displayMetrics.density).toInt()
+
+val Int.dpToPx
+    get() = this.px
 
 fun Resources.getDimensionInt(@DimenRes id: Int): Int {
     return this.getDimension(id).toInt()
