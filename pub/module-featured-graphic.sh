6345e805be12358163b40dd813d8fab037c00ff2
Mathieu Méa <mathieu.mea@gmail.com>
2025-06-09 19:43:45 -0400

    CircleCI: try to fix git diff issue (3)

--- pub/module-featured-graphic.sh
+++ pub/module-featured-graphic.sh
@@ -194,7 +194,11 @@ else
 fi
 sed -i "s/MTCity/$CITY/g" $SOURCE
 sed -i "s/MTStateCountry/$STATE_COUNTRY/g" $SOURCE;
-git -C $ROOT_DIR/commons-android diff --no-color "$SOURCE_GIT_PATH" || echo "no diff";
+if [[ ! -z "${CIRCLECI}" ]]; then
+  echo "CircleCI: no diff";
+else
+  git -C $ROOT_DIR/commons-android diff --no-color "$SOURCE_GIT_PATH" || echo "no diff";
+fi
 echo "> Setting file strings... DONE";
 
 echo "> Running inkscape..."
