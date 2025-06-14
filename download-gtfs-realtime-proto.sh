353202582711673beb8c79a5952bcd842b4589cc
Mathieu Méa <mathieu.mea@gmail.com>
2025-06-13 20:48:20 -0400

    CI: Download GTFS-RT proto workflow (#7)

--- download-gtfs-realtime-proto.sh
+++ download-gtfs-realtime-proto.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source ../commons/commons.sh
+# Keep in sync with ".github/workflows/mt-download-gtfs-rt-proto.yml"
 echo ">> Downloading GTFS Real-Time proto..."
 # URL="https://developers.google.com/static/transit/gtfs-realtime/gtfs-realtime.proto"
 # URL="https://raw.githubusercontent.com/google/transit/master/gtfs-realtime/proto/gtfs-realtime.proto"
