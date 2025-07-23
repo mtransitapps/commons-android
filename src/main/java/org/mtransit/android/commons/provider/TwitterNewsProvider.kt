8b83d65ef788b14f9791d4f849bb1a6021f15304
Mathieu Méa <mathieu.mea@gmail.com>
2025-07-22 20:10:18 -0400

    Twitter news provider > adjust validity / min duration to match rate limits

--- src/main/java/org/mtransit/android/commons/provider/TwitterNewsProvider.kt
+++ src/main/java/org/mtransit/android/commons/provider/TwitterNewsProvider.kt
@@ -53,11 +53,14 @@ class TwitterNewsProvider : NewsProvider() {
         private val VALIDITY_DEBUG_FACTOR = if (BuildConfig.DEBUG) 1L else 2L
         private val VALIDITY_EXPANSIVE_API_FACTOR = VALIDITY_DEBUG_FACTOR * 1L
 
+        // https://docs.x.com/x-api/fundamentals/rate-limits (Basic)
+        // - [GET /2/users/by/username/:username]   500 requests / 24 hours PER APP
+        // - [GET /2/tweets]                        15 requests / 15 mins PER APP
         private val NEWS_MAX_VALIDITY_IN_MS = MAX_CACHE_VALIDITY_MS
-        private val NEWS_VALIDITY_IN_MS = TimeUnit.HOURS.toMillis(12L) * VALIDITY_EXPANSIVE_API_FACTOR
-        private val NEWS_VALIDITY_IN_FOCUS_IN_MS = TimeUnit.MINUTES.toMillis(45L) * VALIDITY_EXPANSIVE_API_FACTOR
-        private val NEWS_MIN_DURATION_BETWEEN_REFRESH_IN_MS = TimeUnit.MINUTES.toMillis(20L) * VALIDITY_EXPANSIVE_API_FACTOR
-        private val NEWS_MIN_DURATION_BETWEEN_REFRESH_IN_FOCUS_IN_MS = TimeUnit.MINUTES.toMillis(5L) * VALIDITY_EXPANSIVE_API_FACTOR
+        private val NEWS_VALIDITY_IN_MS = TimeUnit.HOURS.toMillis(24L) * VALIDITY_EXPANSIVE_API_FACTOR
+        private val NEWS_VALIDITY_IN_FOCUS_IN_MS = TimeUnit.HOURS.toMillis(2L) * VALIDITY_EXPANSIVE_API_FACTOR
+        private val NEWS_MIN_DURATION_BETWEEN_REFRESH_IN_MS = TimeUnit.MINUTES.toMillis(30L) * VALIDITY_EXPANSIVE_API_FACTOR
+        private val NEWS_MIN_DURATION_BETWEEN_REFRESH_IN_FOCUS_IN_MS = TimeUnit.MINUTES.toMillis(15L) * VALIDITY_EXPANSIVE_API_FACTOR
 
         @Suppress("unused")
         val WEB_URL_REGEX = Regex("https?://(www)?(x|twitter)\\.com/(.+)/status/(\\d+)")
