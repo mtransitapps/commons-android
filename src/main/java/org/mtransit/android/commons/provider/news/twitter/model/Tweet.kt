151129fba711300d9fd034d4c325b250643c3726
Mathieu Méa <mathieu.mea@gmail.com>
2025-09-07 18:33:28 -0400

    Twitter news > use native Retrofit instead of big Twitter Java SDK

---
+++ src/main/java/org/mtransit/android/commons/provider/news/twitter/model/Tweet.kt
@@ -0,0 +1,57 @@
+package org.mtransit.android.commons.provider.news.twitter.model
+
+import com.google.gson.annotations.SerializedName
+import java.util.Date
+
+data class Tweet(
+    @SerializedName("id")
+    val id: String,
+    @SerializedName("text")
+    val text: String,
+    @SerializedName("created_at")
+    val createdAt: Date? = null,
+    @SerializedName("author_id")
+    val authorId: String? = null,
+    @SerializedName("in_reply_to_user_id")
+    val inReplyToUserId: String? = null,
+    @SerializedName("attachments")
+    val attachments: TweetAttachments? = null,
+    @SerializedName("entities")
+    val entities: TweetEntities? = null,
+    @SerializedName("lang")
+    val lang: String? = null,
+)
+
+data class TweetAttachments(
+    @SerializedName("media_keys")
+    val mediaKeys: List<String>? = null,
+)
+
+data class TweetEntities(
+    @SerializedName("hashtags")
+    val hashtags: List<TweetEntityTag>? = null,
+    @SerializedName("mentions")
+    val mentions: List<TweetEntityMention>? = null,
+    @SerializedName("urls")
+    val urls: List<TweetEntityURL>? = null
+)
+
+data class TweetEntityTag(
+    @SerializedName("tag")
+    val tag: String // For hashtags, this is the hashtag text; for cashtags, the cashtag text
+)
+
+data class TweetEntityMention(
+    @SerializedName("username")
+    val username: String,
+)
+
+data class TweetEntityURL(
+    @SerializedName("url")
+    val url: String?,
+    @SerializedName("expanded_url")
+    val expandedUrl: String?,
+    @SerializedName("display_url")
+    val displayUrl: String?,
+)
+
---
+++ src/main/java/org/mtransit/android/commons/provider/news/twitter/model/TweetMedia.kt
@@ -0,0 +1,31 @@
+package org.mtransit.android.commons.provider.news.twitter.model
+
+import com.google.gson.annotations.SerializedName
+
+data class TweetMedia(
+    @SerializedName("media_key")
+    val mediaKey: String,
+    @SerializedName("type")
+    val type: String, // e.g., "animated_gif", "photo", "video"
+    @SerializedName("url")
+    val url: String? = null, // For type=photo
+    @SerializedName("preview_image_url")
+    val previewImageUrl: String? = null, // For type=video/animated_gif
+    @SerializedName("variants") // For videos, provides different encodings/bitrates
+    val variants: List<TweetMediaVariant>? = null
+)
+
+enum class TweetMediaType(val type: String) {
+    PHOTO("photo"),
+    VIDEO("video"),
+    ANIMATED_GIF("animated_gif"),
+}
+
+data class TweetMediaVariant(
+    @SerializedName("bit_rate")
+    val bitRate: Int? = null,
+    @SerializedName("content_type")
+    val contentType: String? = null, // e.g., "video/mp4"
+    @SerializedName("url")
+    val url: String? = null
+)
