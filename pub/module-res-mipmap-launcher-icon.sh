f8e084e0d1e1270c9951baf519747712731e552d
Mathieu Méa <mathieu.mea@gmail.com>
2025-10-27 20:39:00 -0400

    Update module-res-mipmap-launcher-icon.sh to hide most DEBUG logs

--- pub/module-res-mipmap-launcher-icon.sh
+++ pub/module-res-mipmap-launcher-icon.sh
@@ -3,6 +3,18 @@ SCRIPT_DIR="$(dirname "$0")"
 source "${SCRIPT_DIR}/../../commons/commons.sh"
 echo ">> Converting Module App Icon to Launcher Icon...";
 
+setIsCI;
+
+DEBUGING=$IS_CI;
+# DEBUGING=true;
+function echoDebug() {
+	if [[ $DEBUGING == true ]]; then
+		echo "$@";
+	else
+		echo -n ".";
+	fi
+}
+
 ROOT_DIR="${SCRIPT_DIR}/../..";
 APP_ANDROID_DIR="$ROOT_DIR/app-android";
 
@@ -12,72 +24,72 @@ if [ ! -f $SOURCE ]; then
     exit 1; #error
 fi
 
-echo "> Creating temporary file...";
-echo " - source: '$SOURCE'";
+echoDebug "> Creating temporary file...";
+echoDebug " - source: '$SOURCE'";
 TEMP="temp_module_app_icon.png";
-echo " - temp: '$TEMP'";
+echoDebug " - temp: '$TEMP'";
 cp $SOURCE $TEMP;
-echo "> Creating temporary file... DONE";
+echoDebug "> Creating temporary file... DONE";
 
-convert -version || (sudo apt-get update && sudo apt-get install -y imagemagick);
+convert -version &> /dev/null || (sudo apt-get update && sudo apt-get install -y imagemagick);
 
 IMAGE_MAGIC_VERSION=$(convert -version);
-echo "> ImageMagic version: $IMAGE_MAGIC_VERSION";
+echoDebug "> ImageMagic version: $IMAGE_MAGIC_VERSION";
 # https://imagemagick.org/script/command-line-options.php
 
 # 75% in https://developer.android.com/google-play/resources/icon-design-specifications
 # 66-85% in https://developer.android.com/guide/practices/ui_guidelines/icon_design_adaptive
 # 90% in https://romannurik.github.io/AndroidAssetStudio/icons-launcher.html
 
-echo "> Cropping...";
+echoDebug "> Cropping...";
 WIDTH=512;
-echo " - width: $WIDTH";
+echoDebug " - width: $WIDTH";
 HEIGHT=512;
-echo " - height: $HEIGHT";
+echoDebug " - height: $HEIGHT";
 CROP="77"; # 66 for bikes ?
 CROP_WIDTH=$((WIDTH * CROP / 100));
 CROP_HEIGHT=$((HEIGHT * CROP / 100));
 OFFSET_X=$(((WIDTH - CROP_WIDTH) / 2 ));
 OFFSET_Y=$(((HEIGHT - CROP_HEIGHT) / 2 ));
-echo " - crop $CROP% width,height: ${CROP_WIDTH}x${CROP_HEIGHT}";
-echo " - offset +X+Y: +${OFFSET_X}+${OFFSET_Y}";
+echoDebug " - crop $CROP% width,height: ${CROP_WIDTH}x${CROP_HEIGHT}";
+echoDebug " - offset +X+Y: +${OFFSET_X}+${OFFSET_Y}";
 convert $TEMP \
 -crop ${CROP_WIDTH}x${CROP_HEIGHT}+${OFFSET_X}+${OFFSET_Y} \
 $TEMP;
 checkResult $?
-echo "> Cropping circle... DONE";
+echoDebug "> Cropping circle... DONE";
 
-echo "> Clipping circle...";
+echoDebug "> Clipping circle...";
 # READ CENTER X,Y
 CX=$(convert $TEMP -format "%[fx:int(w/2)]" info:)
 CY=$(convert $TEMP -format "%[fx:int(h/2)]" info:)
 # FIND POINT ON CIRCLE CIRCUMFERENCE
 PT="0,$CY"
 [ "$CX" -gt "$CY" ] && PT="$CX,0";
-echo " - center X,Y: $CX,$CY";
-echo " - point: $PT";
+echoDebug " - center X,Y: $CX,$CY";
+echoDebug " - point: $PT";
 convert $TEMP \
 \( +clone -fill black -colorize 100% -fill white -draw "circle $CX,$CY $PT" -alpha off \) \
 -compose copyopacity -composite \
 -trim +repage \
 $TEMP;
 checkResult $?
-echo "> Clipping circle... DONE";
+echoDebug "> Clipping circle... DONE";
 
-echo "> Adding padding...";
+echoDebug "> Adding padding...";
 SCALE="90";
 WIDTH_CONTENT=$((CROP_WIDTH * SCALE / 100));
 HEIGHT_CONTENT=$((CROP_HEIGHT * SCALE / 100));
-echo " - scale: $SCALE% ${WIDTH_CONTENT} x ${HEIGHT_CONTENT}";
+echoDebug " - scale: $SCALE% ${WIDTH_CONTENT} x ${HEIGHT_CONTENT}";
 convert $TEMP \
 -background transparent -gravity center \
 -scale ${WIDTH_CONTENT}x${HEIGHT_CONTENT} \
 -extent ${CROP_WIDTH}x${CROP_HEIGHT} \
 $TEMP;
 checkResult $?
-echo "> Adding padding... DONE";
+echoDebug "> Adding padding... DONE";
 
-echo "> Generating mipmap DPI...";
+echoDebug "> Generating mipmap DPI...";
 PARAM="-unsharp 1x4 -strip"; # TODO ?
 RES_DIR="$APP_ANDROID_DIR/src/main/res";
 MIPMAP_NAME="module_app_icon";
@@ -92,12 +104,12 @@ convert ${TEMP} -resize '144x144!' ${PARAM} ${RES_DIR}/mipmap-xxhdpi/${MIPMAP_NA
 checkResult $?
 convert ${TEMP} -resize '192x192!' ${PARAM} ${RES_DIR}/mipmap-xxxhdpi/${MIPMAP_NAME}.png;
 checkResult $?
-echo "> Generating mipmap DPI... DONE";
+echoDebug "> Generating mipmap DPI... DONE";
 
-echo "> Cleaning temporary file '$TEMP'...";
+echoDebug "> Cleaning temporary file '$TEMP'...";
 rm $TEMP;
-echo "> Cleaning temporary file '$TEMP'... DONE";
+echoDebug "> Cleaning temporary file '$TEMP'... DONE";
 
 # TODO later $RES_DIR/mipmap-anydpi-v26/$MIPMAP_NAME.xml + $RES_DIR/mipmap-*/$MIPMAP_NAME_foreground.xml
 
-echo ">> Converting Module App Icon to Launcher Icon... DONE";
+echo -e "\n>> Converting Module App Icon to Launcher Icon... DONE";
