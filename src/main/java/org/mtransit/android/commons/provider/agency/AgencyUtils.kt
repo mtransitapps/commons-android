85521c633e8f602e554bcf3e97a83c098c6f67e0
Mathieu Méa <mathieu.mea@gmail.com>
2026-02-12 15:10:33 -0500

    PR comment

--- src/main/java/org/mtransit/android/commons/provider/agency/AgencyUtils.kt
+++ src/main/java/org/mtransit/android/commons/provider/agency/AgencyUtils.kt
@@ -6,20 +6,30 @@ import org.mtransit.android.commons.R
 object AgencyUtils {
 
     fun getAgencyShortName(context: Context) =
-        context.getString(R.string.poi_agency_short_name).takeIf { it.isNotBlank() }
-            ?: context.getString(R.string.gtfs_rts_short_name).takeIf { it.isNotBlank() } // do not change to avoid breaking compat w/ old modules
-            ?: context.getString(R.string.bike_station_short_name).takeIf { it.isNotBlank() }
+        context.getAgencyString(
+            R.string.poi_agency_short_name,
+            R.string.gtfs_rts_short_name, // do not change to avoid breaking compat w/ old modules
+            R.string.bike_station_short_name
+        )
 
     fun getAgencyColor(context: Context) =
-        context.getString(R.string.poi_agency_color).takeIf { it.isNotBlank() }
-            ?: context.getString(R.string.gtfs_rts_color).takeIf { it.isNotBlank() } // do not change to avoid breaking compat w/ old modules
-            ?: context.getString(R.string.bike_station_color).takeIf { it.isNotBlank() }
+        context.getAgencyString(
+            R.string.poi_agency_color,
+            R.string.gtfs_rts_color, // do not change to avoid breaking compat w/ old modules
+            R.string.bike_station_color
+        )
 
     fun getAgencyAuthority(context: Context) =
-        context.getString(R.string.poi_agency_authority).takeIf { it.isNotBlank() }
-            ?: context.getString(R.string.gtfs_rts_authority).takeIf { it.isNotBlank() } // do not change to avoid breaking compat w/ old modules
-            ?: context.getString(R.string.bike_station_authority).takeIf { it.isNotBlank() }
+        context.getAgencyString(
+            R.string.poi_agency_authority,
+            R.string.gtfs_rts_authority, // do not change to avoid breaking compat w/ old modules
+            R.string.bike_station_authority
+        )
 
+    private fun Context.getAgencyString(vararg resIds: Int): String? =
+        resIds.asSequence()
+            .map { getString(it) }
+            .firstOrNull { it.isNotBlank() }
 
     private var _timeZone: String? = null
 
