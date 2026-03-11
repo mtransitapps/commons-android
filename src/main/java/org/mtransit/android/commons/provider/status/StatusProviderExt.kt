d9a0d682e1306aaa7cbf55c1a4db17661752e91d
Mathieu Méa <mathieu.mea@gmail.com>
2026-03-11 13:34:10 -0400

    cleanup

--- src/main/java/org/mtransit/android/commons/provider/status/StatusProviderExt.kt
+++ src/main/java/org/mtransit/android/commons/provider/status/StatusProviderExt.kt
@@ -20,18 +20,6 @@ fun <P : StatusProviderContract> P.getCachedStatusS(
         this.contentUri,
         buildString {
             append(SqlUtils.getWhereInString(StatusProviderContract.Columns.T_STATUS_K_TARGET_UUID, targetUUIDs))
-            // TODO ? if (FeatureFlags.F_USE_TRIP_IS_FOR_STATUSES) {
-            //     tripIds?.takeIf { it.isNotEmpty() }?.let {
-            //         append(SqlUtils.AND)
-            //         append(
-            //             SqlUtils.getWhereGroup(
-            //                 SqlUtils.OR,
-            //                 SqlUtils.getWhereInString(StatusProviderContract.Columns.T_STATUS_K_TARGET_TRIP_ID, it),
-            //                 SqlUtils.getWhereColumnIsNull(StatusProviderContract.Columns.T_STATUS_K_TARGET_TRIP_ID),
-            //             )
-            //         )
-            //     }
-            // }
         }
     )
 }
