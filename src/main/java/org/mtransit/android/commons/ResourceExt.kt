1e6abff5ea9a5f8a03f760e62033490ed57f510e
Mathieu Méa <mathieu.mea@gmail.com>
2026-05-26 09:46:21 -0400

    Add `Resources.getQuantityText()` w/ format args KT ext

--- src/main/java/org/mtransit/android/commons/ResourceExt.kt
+++ src/main/java/org/mtransit/android/commons/ResourceExt.kt
@@ -1,7 +1,9 @@
 package org.mtransit.android.commons
 
 import android.content.res.Resources
+import androidx.annotation.PluralsRes
 import androidx.annotation.Px
+import androidx.core.text.HtmlCompat
 import kotlin.math.roundToInt
 
 @get:Px
@@ -10,3 +12,8 @@ val Int.dpToPx: Int
 
 val Int.pxToDp: Int
     get() = (this / Resources.getSystem().displayMetrics.density).roundToInt()
+
+fun Resources.getQuantityText(@PluralsRes id: Int, quantity: Int, vararg formatArgs: Any?): CharSequence {
+    val value = getQuantityString(id, quantity, *formatArgs)
+    return HtmlUtils.fromHtmlCompact(value)
+}
