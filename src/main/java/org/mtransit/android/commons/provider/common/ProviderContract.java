b68fccff75954641305f276c99e273308300191f
Mathieu Méa <mathieu.mea@gmail.com>
2026-06-19 14:21:42 -0400

    wip

--- src/main/java/org/mtransit/android/commons/provider/common/ProviderContract.java
+++ src/main/java/org/mtransit/android/commons/provider/common/ProviderContract.java
@@ -37,7 +37,6 @@ public interface ProviderContract extends MTLog.Loggable {
 	abstract class Filter {
 
 		private static final boolean CACHE_ONLY_DEFAULT = false;
-		private static final boolean ASYNC_ONLY_DEFAULT = false;
 		private static final boolean IN_FOCUS_DEFAULT = false;
 
 		@Nullable
@@ -45,20 +44,18 @@ public interface ProviderContract extends MTLog.Loggable {
 		@Nullable
 		private Long cacheValidityInMs = null;
 		@Nullable
-		private Boolean asyncOnly = null;
-		@Nullable
 		private Boolean inFocus = null;
 
 		public boolean isCacheOnlyOrDefault() {
 			return this.cacheOnly == null ? CACHE_ONLY_DEFAULT : this.cacheOnly;
 		}
 
+		@SuppressWarnings("unused")
 		@Nullable
 		public Boolean getCacheOnlyOrNull() {
 			return this.cacheOnly;
 		}
 
-		@NonNull
 		public void setCacheOnly(@Nullable Boolean cacheOnly) {
 			this.cacheOnly = cacheOnly;
 		}
@@ -68,28 +65,16 @@ public interface ProviderContract extends MTLog.Loggable {
 			return this.cacheValidityInMs;
 		}
 
-		@NonNull
+		@SuppressWarnings("unused")
 		public void setCacheValidityInMs(@Nullable Long cacheValidityInMs) {
 			this.cacheValidityInMs = cacheValidityInMs;
-		}
-
-		public void setAsyncOnly(@Nullable Boolean asyncOnly) {
-			this.asyncOnly = asyncOnly;
-		}
-
-		public boolean isAsyncOnlyOrDefault() {
-			return this.asyncOnly == null ? ASYNC_ONLY_DEFAULT : this.asyncOnly;
-		}
-
-		@Nullable
-		public Boolean getAsyncOnlyOrNull() {
-			return this.asyncOnly;
 		}
 
 		public boolean isInFocusOrDefault() {
 			return this.inFocus == null ? IN_FOCUS_DEFAULT : this.inFocus;
 		}
 
+		@SuppressWarnings("unused")
 		@Nullable
 		public Boolean getInFocusOrNull() {
 			return this.inFocus;
@@ -101,7 +86,6 @@ public interface ProviderContract extends MTLog.Loggable {
 
 		private static final String JSON_CACHE_ONLY = "cacheOnly";
 		private static final String JSON_CACHE_VALIDITY_IN_MS = "cacheValidityInMs";
-		private static final String JSON_ASYNC_ONLY = "asyncOnly";
 		private static final String JSON_IN_FOCUS = "inFocus";
 
 		@Nullable
@@ -111,17 +95,14 @@ public interface ProviderContract extends MTLog.Loggable {
 		}
 
 		public static void toJSON(@NonNull Filter filter, @NonNull JSONObject json) throws JSONException {
-			if (filter.getCacheOnlyOrNull() != null) {
-				json.put(JSON_CACHE_ONLY, filter.getCacheOnlyOrNull());
+			if (filter.cacheOnly != null) {
+				json.put(JSON_CACHE_ONLY, filter.cacheOnly);
 			}
-			if (filter.getCacheValidityInMsOrNull() != null) {
-				json.put(JSON_CACHE_VALIDITY_IN_MS, filter.getCacheValidityInMsOrNull());
+			if (filter.cacheValidityInMs != null) {
+				json.put(JSON_CACHE_VALIDITY_IN_MS, filter.cacheValidityInMs);
 			}
-			if (filter.getAsyncOnlyOrNull() != null) {
-				json.put(JSON_ASYNC_ONLY, filter.getAsyncOnlyOrNull());
-			}
-			if (filter.getInFocusOrNull() != null) {
-				json.put(JSON_IN_FOCUS, filter.getInFocusOrNull());
+			if (filter.inFocus != null) {
+				json.put(JSON_IN_FOCUS, filter.inFocus);
 			}
 		}
 
@@ -132,20 +113,16 @@ public interface ProviderContract extends MTLog.Loggable {
 			if (json.has(JSON_CACHE_VALIDITY_IN_MS)) {
 				filter.cacheValidityInMs = json.getLong(JSON_CACHE_VALIDITY_IN_MS);
 			}
-			if (json.has(JSON_ASYNC_ONLY)) {
-				filter.asyncOnly = json.getBoolean(JSON_ASYNC_ONLY);
-			}
 			if (json.has(JSON_IN_FOCUS)) {
 				filter.inFocus = json.getBoolean(JSON_IN_FOCUS);
 			}
 		}
 
 		@NonNull
-		public String toStringParts() {
+		protected String toStringParts() {
 			final StringBuilder sb = new StringBuilder();
 			if (this.cacheOnly != null) sb.append("cacheOnly:").append(this.cacheOnly).append(',');
 			if (this.cacheValidityInMs != null) sb.append("cacheValidityInMs:").append(MTLog.formatDuration(this.cacheValidityInMs)).append(',');
-			if (this.asyncOnly != null) sb.append("asyncOnly:").append(this.asyncOnly).append(',');
 			if (this.inFocus != null) sb.append("inFocus:").append(this.inFocus).append(",");
 			return sb.toString();
 		}
