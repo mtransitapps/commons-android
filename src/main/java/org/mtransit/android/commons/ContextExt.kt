2cfd1d3221c57b42b58b9608765121d2ea7c650d
Mathieu Méa <mathieu.mea@gmail.com>
2026-06-23 13:29:04 -0400

    Fix spread operator usage in getText function

--- src/main/java/org/mtransit/android/commons/ContextExt.kt
+++ src/main/java/org/mtransit/android/commons/ContextExt.kt
@@ -17,7 +17,7 @@ fun Context.dimensionFromAttribute(attribute: Int, defaultValue: Int = -1): Int 
 fun Context.getStringRes(@StringRes resId: Int, vararg formatArgsStringRes: Int): String =
     getString(resId, formatArgsStringRes.map { getString(it) })
 
-fun Context.getText(@StringRes resId: Int, vararg formatArgs: Any?): CharSequence = resources.getText(resId, formatArgs)
+fun Context.getText(@StringRes resId: Int, vararg formatArgs: Any?): CharSequence = resources.getText(resId, *formatArgs)
 
 fun PowerManager.isIgnoringBatteryOpt(packageName: String): Boolean {
     return this.isIgnoringBatteryOptimizations(packageName)
