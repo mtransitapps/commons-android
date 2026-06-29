e05544329c13511790c02c0b10a90f4a18f5f0d8
Mathieu Méa <mathieu.mea@gmail.com>
2026-06-29 13:51:41 -0400

    wip

--- src/main/java/org/mtransit/android/commons/provider/status/StatusProviderContract.kt
+++ src/main/java/org/mtransit/android/commons/provider/status/StatusProviderContract.kt
@@ -75,17 +75,16 @@ interface StatusProviderContract : ProviderContract {
             private const val JSON_TARGET_UUID = "target"
 
             @JvmStatic
-            fun from(
-                poi: POI,
+            fun POI.from(
                 inFocus: Boolean?,
                 scheduleBehindInMs: Long?,
                 scheduleMaxDataRequests: Int?,
                 scheduleIncludeCancelledTimestamps: Boolean?,
                 getAppPkg: () -> String?
-            ): Filter? = when (poi.statusType) {
+            ): Filter? = when (this.statusType) {
                 POI.ITEM_STATUS_TYPE_NONE -> null
                 POI.ITEM_STATUS_TYPE_SCHEDULE -> {
-                    (poi as? RouteDirectionStop)?.let {
+                    (this as? RouteDirectionStop)?.let {
                         ScheduleStatusFilter(
                             inFocus = inFocus,
                             routeDirectionStop = it,
@@ -100,13 +99,13 @@ interface StatusProviderContract : ProviderContract {
                 }
 
                 POI.ITEM_STATUS_TYPE_AVAILABILITY_PERCENT -> AvailabilityPercentStatusFilter(
-                    targetUUID = poi.uuid,
+                    targetUUID = uuid,
                     inFocus = inFocus,
                 )
 
                 POI.ITEM_STATUS_TYPE_APP -> {
                     getAppPkg()?.let { pkg ->
-                        AppStatusFilter(inFocus = inFocus, targetUUID = poi.uuid, pkg = pkg)
+                        AppStatusFilter(inFocus = inFocus, targetUUID = uuid, pkg = pkg)
                     } ?: run {
                         MTLog.w(LOG_TAG, "App status filter w/o '$poi'!")
                         null
@@ -114,7 +113,7 @@ interface StatusProviderContract : ProviderContract {
                 }
 
                 else -> {
-                    MTLog.w(LOG_TAG, "Unexpected status type '${poi.statusType}' for filter!")
+                    MTLog.w(LOG_TAG, "Unexpected status type '${this.statusType}' for filter!")
                     null
                 }
             }
