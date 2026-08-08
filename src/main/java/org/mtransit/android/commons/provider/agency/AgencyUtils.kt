259dabf8ad9e0ea2c1ce679dd9312e1a1fb58eb1
Mathieu Méa <mathieu.mea@gmail.com>
2026-08-08 17:33:35 -0400

    wip

--- src/main/java/org/mtransit/android/commons/provider/agency/AgencyUtils.kt
+++ src/main/java/org/mtransit/android/commons/provider/agency/AgencyUtils.kt
@@ -1,10 +1,15 @@
 package org.mtransit.android.commons.provider.agency
 
 import android.content.Context
+import org.mtransit.android.commons.MTLog
 import org.mtransit.android.commons.R
 import java.util.TimeZone
 
-object AgencyUtils {
+object AgencyUtils : MTLog.Loggable {
+
+    private val LOG_TAG: String = AgencyUtils::class.java.simpleName
+
+    override fun getLogTag() = LOG_TAG
 
     fun getAgencyShortName(context: Context) =
         context.getAgencyString(
@@ -32,13 +37,14 @@ object AgencyUtils {
             .map { getString(it) }
             .firstOrNull { it.isNotBlank() }
 
-    private val _defaultTimeZoneId: String by lazy { TimeZone.getDefault().id }
-
     @JvmStatic
-    fun getAgencyTimeZoneId(context: Context) =
+    fun getAgencyTimeZoneId(context: Context): String =
         context.getAgencyString(
             R.string.poi_agency_timezone,
             R.string.gtfs_rts_timezone, // do not change to avoid breaking compat w/ old modules
             R.string.bike_station_timezone,
-        ) ?: _defaultTimeZoneId
+        ) ?: run {
+            MTLog.w(LOG_TAG, "Impossible to read agency timezone! (using device timezone)")
+            TimeZone.getDefault().id
+        }
 }
